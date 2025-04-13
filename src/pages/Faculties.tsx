16009a1cf7c991beb237dfab1a8e35f38da62584
PallaviKlmfy <pallavi.solday@klaimify.in>
2025-04-13 14:44:52 +0530

    [FIX] -  favicon icon and build issues

--- src/pages/Faculties.tsx
+++ src/pages/Faculties.tsx
@@ -69,6 +69,7 @@ const Faculties: React.FC = () => {
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
       {FacultyMembers.map((member: FacultyMember, index: number) => (
         <FacultyCard
+        key={index}
         image={member.image}
         index={index}
         name={member.name}
--- src/pages/PrincipalsNote.tsx
+++ src/pages/PrincipalsNote.tsx
@@ -14,7 +14,7 @@ const breadcrumbs: Breadcrumb[] = [
 const PrincipalsNote = () => {
     return (
         <>
-            <Banner backgroundImage="/images/Section.png" pageTitle="Principal’s Note" breadcrumbs={breadcrumbs} />
+            <Banner backgroundImage="/images/Section.png" pageTitle="Principal&apos;s Note" breadcrumbs={breadcrumbs} />
 
             <section className="max-sm:px-[10px] sm:px-[30px] lg:px-[50px] xl:px-[100px] md:py-[50px] py-[24px] bg-[#F6F6FF]">
                 <SubTitle subTitle="Welcome from," textAlign="left" />
@@ -26,14 +26,14 @@ const PrincipalsNote = () => {
                     description={
                         <>
                             <p className="mb-4 text-[18px] text-[#000] text-justify">Dear Parents, Students, and Esteemed Community Members,</p>
-                            <p className="mb-4 text-[18px] text-[#000] text-justify">With great pleasure and honour, I welcome you to Achievers International School, where our commitment to excellence and holistic development sets the foundation for our student's success. As the Principal, I am privileged to lead a community that believes in nurturing every child's unique talents and aspirations. Our mission is to provide an environment where academic rigour harmoniously blends with character-building, creativity, and critical thinking.</p>
+                            <p className="mb-4 text-[18px] text-[#000] text-justify">With great pleasure and honour, I welcome you to Achievers International School, where our commitment to excellence and holistic development sets the foundation for our student&apos;s success. As the Principal, I am privileged to lead a community that believes in nurturing every child&apos;s unique talents and aspirations. Our mission is to provide an environment where academic rigour harmoniously blends with character-building, creativity, and critical thinking.</p>
                             <p className="mb-4 text-[18px] text-[#000] text-justify">At Achievers International, we take pride in being pioneers of AI-integrated classrooms. Our recognition as the No. 1 CBSE school in the region is a testament to our pursuit of innovation. Our dedicated educators strive to instil a lifelong love for learning, empowering students to explore their passions, embrace challenges, and develop resilience. We recognise that education goes beyond textbooks, and we offer a rich curriculum balanced with extracurricular activities to encourage holistic growth.</p>
-                            <p className="mb-4 text-[18px] text-[#000] text-justify">We also believe that a strong partnership between school and home is crucial for our students' success. Your involvement and support are invaluable as we work together to create a nurturing and stimulating environment.</p>
+                            <p className="mb-4 text-[18px] text-[#000] text-justify">We also believe that a strong partnership between school and home is crucial for our students&apos; success. Your involvement and support are invaluable as we work together to create a nurturing and stimulating environment.</p>
                         </>
                     }
                 />
                 <div>
-                    <p className="mb-4 text-[18px] text-[#000] text-justify">This website serves as a comprehensive resource for information about our school's programs, policies, and upcoming events. We invite you to explore its various sections and stay connected with our vibrant school community.</p>
+                    <p className="mb-4 text-[18px] text-[#000] text-justify">This website serves as a comprehensive resource for information about our school&apos;s programs, policies, and upcoming events. We invite you to explore its various sections and stay connected with our vibrant school community.</p>
                     <p className="mb-4 text-[18px] text-[#000] text-justify">As we embark on this journey together, let us remember that education is a shared endeavour—one that shapes confident individuals and compassionate citizens.</p>
                     <p className="mb-4 text-[18px] text-[#000] text-justify"><strong>Warm regards,</strong></p>
                     <p className="mb-2 text-[20px] text-[#000] text-justify"><strong>Ashok Tiwari</strong></p>
