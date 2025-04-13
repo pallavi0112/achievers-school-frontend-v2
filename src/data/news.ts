f2ad7f0d82bbbd4c8c468be3a403952bde09224d
Nishan kumar <nishan1805@gmail.com>
2025-04-13 18:44:45 +0530

    photo changes

--- src/data/news.ts
+++ src/data/news.ts
@@ -18,11 +18,11 @@ const news: NewsInterface[] = [
     {
         title: "Best Emerging School Award",
         description: "The school has Hi-Tech smart class teaching system in place to foster effective learning through audio and video.",
-        image: "/images/N3.png",
+        image: "/images/news 17 jan.jpg",
         date: "2025-Jan-17",
     },
     {
-        title: "School Received Recognition up to 12th Grade",
+        title: "Received Recognition up to 12th Grade",
         description: "The school has Hi-Tech smart class teaching system in place to foster effective learning through audio and video.",
         image: "/images/6 nov 23.jpg",
         date: "2023-Nov-06",
