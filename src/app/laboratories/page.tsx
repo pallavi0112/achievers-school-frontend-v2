8b45757ed9b98d23be04fcadee391e4915eefaec
Ghulam Rabbani Ansari <ghulamrabbani883@gmail.com>
2025-04-27 01:11:23 +0530

    changes laboratory page

--- src/app/laboratories/page.tsx
+++ src/app/laboratories/page.tsx
@@ -20,7 +20,7 @@ const Laboratories = () => {
         pageTitle="Laboratories"
         breadcrumbs={breadcrumbs}
       />
-      <Links/>
+      <Links />
       <section className="max-sm:px-[10px] sm:px-[30px] lg:px-[50px] xl:px-[100px] md:py-[50px] py-[24px] bg-[#F6F6FF]">
         <SubTitle subTitle="Discover Our" textAlign="left" />
         <Title title="Laboratories" textAlign="left" />
@@ -30,28 +30,59 @@ const Laboratories = () => {
           imageLeft={true}
           description={
             <>
-              <div className="text-[#525252] font-inter text-[16px] md:text-[18px] font-normal font-medium leading-[140%]">
+              <SubTitle subTitle='Physics Laboratory' textAlign='left' />
+              <div className="text-[#525252] mt-5 font-inter text-[16px] md:text-[18px] font-normal font-medium leading-[140%]">
                 <p className="mb-8">
-                  Hands-on experience is at the heart of scientific learning at
-                  Achievers International School. Our labs are designed to
-                  ignite curiosity and enhance conceptual understanding through
-                  active exploration and experimentation.
+                  Step into a world where imagination meets scientific inquiry.
                 </p>
                 <p className="mb-8">
-                  From Biology to Chemistry and Physics, our well-equipped
-                  laboratories provide a safe and stimulating environment for
-                  students to observe, analyze, and discover scientific
-                  principles in action. Supervised by qualified faculty,
-                  students engage in practical work that reinforces classroom
-                  learning and fosters a spirit of inquiry.
+                  Our Physics Lab is a vibrant center for exploration, equipped with modern instruments that allow students to experiment with light, sound, motion, electricity, and more. Under expert supervision, students learn to design and execute experiments that explain the physical phenomena governing the universe.
                 </p>
-                <p>
-                  We ensure that our lab sessions go beyond routine experiments,
-                  encouraging critical thinking, collaborative learning, and
-                  real-world application. Safety protocols are strictly
-                  followed, and our facilities are regularly updated to align
-                  with evolving curriculum standards.
+                <p className="mb-8">
+                  By conducting real-time experiments, they enhance their analytical skills, develop precision, and foster a deep, lasting understanding of concepts like mechanics, optics, electromagnetism, and thermodynamics. Safety protocols are strictly followed, ensuring a secure yet stimulating environment for budding physicists.
                 </p>
+
+              </div>
+            </>
+          }
+        />
+
+        {/* SOmething adding */}
+        <ContentCard
+          image="/images/Content.png"
+          alt="Welcome to AIS"
+          imageLeft={false}
+          description={
+            <>
+              <SubTitle subTitle='Chemistry Laboratory' textAlign='left' />
+              <div className="text-[#525252] mt-5 font-inter text-[16px] md:text-[18px] font-normal font-medium leading-[140%]">
+                <p className="mb-8">
+                  Where reactions spark curiosity and every solution leads to new discovery.                </p>
+                <p className="mb-8">
+                  Our Chemistry Lab offers a safe, spacious, and modern environment where students dive into the world of atoms, molecules, and compounds. Through carefully guided experiments, students learn about chemical reactions, organic and inorganic chemistry, acids, bases, and much more.                </p>
+                <p className="mb-8">
+                  Beyond just observing changes, they are trained to interpret results, make predictions, and understand the real-world applications of chemistry. All experiments are conducted under strict safety measures, with proper protective gear and close supervision to ensure a safe and engaging learning experience.                </p>
+
+              </div>
+            </>
+          }
+        />
+
+        <ContentCard
+          image="/images/Content.png"
+          alt="Welcome to AIS"
+          imageLeft={true}
+          description={
+            <>
+              <SubTitle subTitle='Mathematics Laboratory' textAlign='left' />
+              <div className="text-[#525252] mt-5 font-inter text-[16px] md:text-[18px] font-normal font-medium leading-[140%]">
+                <p className="mb-8">
+                  Making numbers tangible and mathematics magical.   </p>             
+                  <p className="mb-8">
+                  The Mathematics Lab at Achievers International School is a unique space where students experience the joy of learning mathematics through hands-on activities, puzzles, games, and models.                </p>
+                  <p className="mb-8">
+                  Rather than relying solely on theoretical study, students interact with mathematical concepts like geometry, algebra, trigonometry, and statistics in a visual and practical manner. This active approach strengthens problem-solving abilities, logical reasoning, and fosters a positive attitude towards mathematics — turning challenges into exciting adventures.             </p>
+
               </div>
             </>
           }
